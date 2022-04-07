6fdcba772e9c9e6ca51daabf4276348ec502551f
Atanas Ivanov <naskuuu123@gmail.com>
2022-04-07 23:48:59 +0300

    add dummy controller with mapping

--- week_06/theaterticketsystem/theaterticketsystem/src/main/java/com/fmi/web/theaterticketsystem/model/User.java
+++ week_06/theaterticketsystem/theaterticketsystem/src/main/java/com/fmi/web/theaterticketsystem/model/User.java
@@ -5,27 +5,26 @@ public class User {
   private String userName;
   private String email;
 
-  public Long getId() {
-    return id;
+  // works when using post request - deserialization :)
+  public User(Long id, String userName, String email) {
+    this.id = id;
+    this.userName = userName;
+    this.email = email;
   }
 
-  public void setId(final Long id) {
-    this.id = id;
+  public Long getId() {
+    return id;
   }
 
   public String getUserName() {
     return userName;
   }
 
-  public void setUserName(final String userName) {
-    this.userName = userName;
-  }
-
   public String getEmail() {
     return email;
   }
 
-  public void setEmail(final String email) {
-    this.email = email;
+  public void setId(Long id) {
+    this.id = id;
   }
 }
